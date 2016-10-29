6b1df7f60eee97e0d408695a91e7399a9361e9a5
Xuanyu Wang <xuanyu.wang@outlook.com>
2016-10-29 10:23:20 -0300

    feature: get content of title in a string

---
+++ XMLParsing.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+import Foundation
+import XCPlayground
+
+// By default Playgrounds execute the code in each page, from top-to-bottom,
+// then they stop executing.
+// By setting the needsIndefiniteExecution to be true,
+// we allow any asynchronous code we have in our Playground to continue running indefinitely.
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+
+class XMLParser: NSObject, NSXMLParserDelegate {
+    var currentElement = "" // current element during parsing
+    var contentOfCurrentElement = ""
+    
+    func startParsingWithContentsOfURL(rssURL: NSURL) {
+        let parser = NSXMLParser(contentsOfURL: rssURL)
+        parser!.delegate = self
+        parser!.parse()
+    }
+    
+    // called every time the parser finds a <key>
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        //print(elementName)
+        currentElement = elementName
+    }
+    
+    //called every time the parser enters a <key> and it will stop on line breaks
+    func parser(parser: NSXMLParser, foundCharacters str: String) {
+        if currentElement == "title" {
+            contentOfCurrentElement += str
+        }
+        
+    }
+
+    // called every time the parser finds a </key>
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if currentElement == "title" {
+            print(contentOfCurrentElement)
+        }
+       currentElement = ""
+        contentOfCurrentElement = ""
+    }
+    
+    
+    
+    // called when the parser finished the document
+    func parserDidEndDocument(parser: NSXMLParser) {
+        print("parse has ended")
+    }
+    
+}
+
+let url = NSURL(string: "https://weather.gc.ca/rss/city/ns-19_e.xml")
+var xmlParser = XMLParser()
+xmlParser.startParsingWithContentsOfURL(url!)
